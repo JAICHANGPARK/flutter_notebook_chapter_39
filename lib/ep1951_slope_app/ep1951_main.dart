7a222570a1db852ca75221e09e37a14a3845cb04
JAICHANGPARK <aristojeff@gmail.com>
2024-07-23 20:49:23 +0900

    Create ep1951_main.dart

---
+++ lib/ep1951_slope_app/ep1951_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(SlopeMobileApp());
+}
+
+class SlopeMobileApp extends StatelessWidget {
+  const SlopeMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
