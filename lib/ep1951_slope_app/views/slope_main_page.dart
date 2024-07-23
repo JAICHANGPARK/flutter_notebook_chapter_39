99ee1a7f3db5aa99c441531b0312c53bff815dbd
JAICHANGPARK <aristojeff@gmail.com>
2024-07-23 20:50:50 +0900

    Create slope_main_page.dart

---
+++ lib/ep1951_slope_app/views/slope_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SlopeMainPage extends StatefulWidget {
+  const SlopeMainPage({super.key});
+
+  @override
+  State<SlopeMainPage> createState() => _SlopeMainPageState();
+}
+
+class _SlopeMainPageState extends State<SlopeMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
