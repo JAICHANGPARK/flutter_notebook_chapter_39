e29a2db6614ee310aa1abb5c186cff93b970b24d
JAICHANGPARK <aristojeff@gmail.com>
2024-07-25 20:12:35 +0900

    Update slope_detail_page.dart

--- lib/ep1951_slope_app/views/slope_detail_page.dart
+++ lib/ep1951_slope_app/views/slope_detail_page.dart
@@ -72,27 +72,51 @@ class _SlopeDetailPageState extends State<SlopeDetailPage> {
                     child: Column(
                       children: [
                         Gap(24),
-                        Row(
-                          children: [
-                            Container(
-                              width: 200,
-                              color: Colors.blue,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text("650 km"),
-                                  Text("total tracks"),
-                                  Text("3200 km"),
-                                  Text("total tracks"),
-                                  Text("650 km"),
-                                  Text("total tracks"),
-                                ],
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 200,
+                                height: 240,
+                                color: Colors.blue,
                               ),
-                            )
-                          ],
+                              Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text("650 km"),
+                                    Text("total tracks"),
+                                    Text("3200 m"),
+                                    Text("maximum height"),
+                                    Text("1800 m"),
+                                    Text("drop"),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Expanded(
+                          child: Row(
+                            children: [
+                              Container(
+                          
+                                decoration: BoxDecoration(
+                                  color: Colors.black,
+                                ),
+                                child: IconButton(
+                                  icon: Icon(Icons.bookmark_added_outlined),
+                                  onPressed: (){},
+                                  color: Colors.white,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Spacer(),
+                        Container(
+                          decoration: BoxDecoration(),
                         )
                       ],
                     ),
