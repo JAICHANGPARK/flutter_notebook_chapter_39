56895b3a17f5100eeda88b7dfb9a2d1d7ee0401f
JAICHANGPARK <aristojeff@gmail.com>
2024-07-25 20:15:19 +0900

    Update slope_detail_page.dart

--- lib/ep1951_slope_app/views/slope_detail_page.dart
+++ lib/ep1951_slope_app/views/slope_detail_page.dart
@@ -101,6 +101,7 @@ class _SlopeDetailPageState extends State<SlopeDetailPage> {
                         Expanded(
                           child: Row(
                             children: [
+                              Gap(24),
                               Container(
                                 decoration: BoxDecoration(
                                   color: Colors.black,
@@ -116,13 +117,14 @@ class _SlopeDetailPageState extends State<SlopeDetailPage> {
                                 child: Text(
                                   """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                                 """,
-                                  
+                                  maxLines: 10,
                                 ),
                               ),
                               Gap(24),
                             ],
                           ),
                         ),
+                        Gap(24),
                         Container(
                           decoration: BoxDecoration(
                             color: Colors.grey[200]!,
