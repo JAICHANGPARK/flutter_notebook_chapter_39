c2178bcb0227ed5054b312895d77e9c638136ed3
JAICHANGPARK <aristojeff@gmail.com>
2024-07-25 20:01:21 +0900

    Update slope_detail_page.dart

---
+++ lib/ep1951_slope_app/views/slope_detail_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SlopeDetailPage extends StatefulWidget {
+  const SlopeDetailPage({super.key});
+
+  @override
+  State<SlopeDetailPage> createState() => _SlopeDetailPageState();
+}
+
+class _SlopeDetailPageState extends State<SlopeDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
