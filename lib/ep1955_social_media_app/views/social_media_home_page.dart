590e4948b1dfe3521be8b87c2af242345326345b
JAICHANGPARK <aristojeff@gmail.com>
2024-07-26 19:21:55 +0900

    Update social_media_home_page.dart

---
+++ lib/ep1955_social_media_app/views/social_media_home_page.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:flutter/material.dart';
+
+class SocialMediaHomePage extends StatefulWidget {
+  const SocialMediaHomePage({super.key});
+
+  @override
+  State<SocialMediaHomePage> createState() => _SocialMediaHomePageState();
+}
+
+class _SocialMediaHomePageState extends State<SocialMediaHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        
+      ),
+      body: Column(
+
+        children: [],
+      ),
+    );
+  }
+}
