c128ba8589f45d5a7466ffa36ce52a8f609eed64
JAICHANGPARK <aristojeff@gmail.com>
2024-07-26 19:03:12 +0900

    Update slope_detail_page.dart

--- lib/ep1951_slope_app/views/slope_detail_page.dart
+++ lib/ep1951_slope_app/views/slope_detail_page.dart
@@ -149,12 +149,18 @@ class _SlopeDetailPageState extends State<SlopeDetailPage> {
           Positioned(
             left: 16,
             top: 260,
+            
             child: Container(
               height: 280,
               width: 200,
+              padding: EdgeInsets.all(8),
               color: Colors.white,
               child: Container(
-                child: Image.network("src"),
+                child: Image.network(
+                  "https://cdn.pixabay.com/photo/2017/02/14/03/03/ama-dablam-2064522_1280.jpg",
+                  fit: BoxFit.cover,
+                ),
+
               ),
             ),
           ),
