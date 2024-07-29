69cbc5ce68566363b058190b55ff60412df2b6f5
JAICHANGPARK <aristojeff@gmail.com>
2024-07-29 18:44:52 +0900

    Update social_media_home_page.dart

--- lib/ep1955_social_media_app/views/social_media_home_page.dart
+++ lib/ep1955_social_media_app/views/social_media_home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_39/ep1955_social_media_app/views/social_media_profile_page.dart';
 import 'package:gap/gap.dart';
 
 class SocialMediaHomePage extends StatefulWidget {
@@ -9,6 +10,8 @@ class SocialMediaHomePage extends StatefulWidget {
 }
 
 class _SocialMediaHomePageState extends State<SocialMediaHomePage> {
+  int pageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,128 +35,137 @@ class _SocialMediaHomePageState extends State<SocialMediaHomePage> {
           const Gap(12),
         ],
       ),
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.bottomCenter,
-                end: Alignment.topCenter,
-                colors: [
-                  Colors.black,
-                  Colors.white.withOpacity(.05),
-                  Colors.white.withOpacity(.1),
-                ],
-                stops: [0.8, 0.2, .1],
-              ),
-            ),
-            padding: const EdgeInsets.all(16),
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const CircleAvatar(
-                  radius: 22,
+      body: IndexedStack(
+        index: pageIndex,
+        children: [
+          ListView.builder(
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [
+                      Colors.black,
+                      Colors.white.withOpacity(.05),
+                      Colors.white.withOpacity(.1),
+                    ],
+                    stops: [0.8, 0.2, .1],
+                  ),
                 ),
-                const Gap(16),
-                Expanded(
-                  child: Column(
-                    children: [
-                      const Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const CircleAvatar(
+                      radius: 22,
+                    ),
+                    const Gap(16),
+                    Expanded(
+                      child: Column(
                         children: [
-                          Text(
-                            "Dream",
-                            style: TextStyle(
-                              color: Colors.white,
-                            ),
-                          ),
-                          Text(
-                            "2h",
-                            style: TextStyle(
-                              color: Colors.grey,
-                            ),
-                          ),
-                        ],
-                      ),
-                      const Text(
-                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                        maxLines: 2,
-                        style: TextStyle(
-                          color: Colors.grey,
-                        ),
-                      ),
-                      if (index != 0)
-                        Container(
-                          margin: const EdgeInsets.symmetric(vertical: 12),
-                          height: 240,
-                          child: Row(
+                          const Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              Expanded(
-                                child: Container(
-                                  decoration: const BoxDecoration(
-                                    borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(4),
-                                      bottomLeft: Radius.circular(4),
-                                    ),
-                                    color: Colors.orange,
-                                  ),
+                              Text(
+                                "Dream",
+                                style: TextStyle(
+                                  color: Colors.white,
                                 ),
                               ),
-                              const Gap(4),
-                              Expanded(
-                                child: Container(
-                                  decoration: const BoxDecoration(
-                                    borderRadius: BorderRadius.only(
-                                      topRight: Radius.circular(4),
-                                      bottomRight: Radius.circular(4),
-                                    ),
-                                    color: Colors.orange,
-                                  ),
+                              Text(
+                                "2h",
+                                style: TextStyle(
+                                  color: Colors.grey,
                                 ),
                               ),
                             ],
                           ),
-                        ),
-                      const Row(
-                        children: [
-                          Icon(
-                            Icons.favorite,
-                            color: Colors.red,
-                          ),
-                          Gap(6),
-                          Text(
-                            "40.9K",
+                          const Text(
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                            maxLines: 2,
                             style: TextStyle(
                               color: Colors.grey,
                             ),
                           ),
-                          Gap(12),
-                          Icon(
-                            Icons.chat_outlined,
-                            color: Colors.grey,
-                          ),
-                          Gap(6),
-                          Text(
-                            "1.3K",
-                            style: TextStyle(
-                              color: Colors.grey,
+                          if (index != 0)
+                            Container(
+                              margin: const EdgeInsets.symmetric(vertical: 12),
+                              height: 240,
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Container(
+                                      decoration: const BoxDecoration(
+                                        borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(4),
+                                          bottomLeft: Radius.circular(4),
+                                        ),
+                                        color: Colors.orange,
+                                      ),
+                                    ),
+                                  ),
+                                  const Gap(4),
+                                  Expanded(
+                                    child: Container(
+                                      decoration: const BoxDecoration(
+                                        borderRadius: BorderRadius.only(
+                                          topRight: Radius.circular(4),
+                                          bottomRight: Radius.circular(4),
+                                        ),
+                                        color: Colors.orange,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
+                          const Row(
+                            children: [
+                              Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                              ),
+                              Gap(6),
+                              Text(
+                                "40.9K",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              Gap(12),
+                              Icon(
+                                Icons.chat_outlined,
+                                color: Colors.grey,
+                              ),
+                              Gap(6),
+                              Text(
+                                "1.3K",
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              Spacer(),
+                              Icon(
+                                Icons.bookmark,
+                                color: Colors.grey,
+                              )
+                            ],
                           ),
-                          Spacer(),
-                          Icon(
-                            Icons.bookmark,
-                            color: Colors.grey,
-                          )
                         ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
-            ),
-          );
-        },
+              );
+            },
+          ),
+          Container(),
+          Container(),
+          Container(),
+          const SocialMediaProfilePage(),
+        ],
       ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.black,
@@ -196,7 +208,13 @@ class _SocialMediaHomePageState extends State<SocialMediaHomePage> {
               color: Colors.grey,
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+
+                setState(() {
+                  pageIndex = 4;
+                });
+
+              },
               icon: const Icon(
                 Icons.account_circle_rounded,
               ),
