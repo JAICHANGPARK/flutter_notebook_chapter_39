411b8960c306aa5922412cdd069c11176e5d7f9c
JAICHANGPARK <aristojeff@gmail.com>
2024-07-31 22:13:01 +0900

    Create ep1959_main.dart

---
+++ lib/ep1959_draft_bot_app/ep1959_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+class DraftBotApp extends StatelessWidget {
+  const DraftBotApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
