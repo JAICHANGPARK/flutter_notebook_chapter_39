94858004c8aee33ee3a5870788fb071c5efd4cb8
JAICHANGPARK <aristojeff@gmail.com>
2024-07-31 22:14:40 +0900

    Create draft_bot_home_page.dart

---
+++ lib/ep1959_draft_bot_app/views/draft_bot_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class DraftBotHomePage extends StatefulWidget {
+  const DraftBotHomePage({super.key});
+
+  @override
+  State<DraftBotHomePage> createState() => _DraftBotHomePageState();
+}
+
+class _DraftBotHomePageState extends State<DraftBotHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
