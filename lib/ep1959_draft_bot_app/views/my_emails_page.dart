51e681d36f1bbcd09d846ce618e5ffc0f065970e
JAICHANGPARK <aristojeff@gmail.com>
2024-08-03 08:43:15 +0900

    Update my_emails_page.dart

---
+++ lib/ep1959_draft_bot_app/views/my_emails_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class MyEmailsPage extends StatefulWidget {
+  const MyEmailsPage({super.key});
+
+  @override
+  State<MyEmailsPage> createState() => _MyEmailsPageState();
+}
+
+class _MyEmailsPageState extends State<MyEmailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
