7a471c70412326d7fa2c7739aa16499e5767474a
JAICHANGPARK <aristojeff@gmail.com>
2024-08-03 08:49:03 +0900

    Update my_emails_page.dart

--- lib/ep1959_draft_bot_app/views/my_emails_page.dart
+++ lib/ep1959_draft_bot_app/views/my_emails_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class MyEmailsPage extends StatefulWidget {
   const MyEmailsPage({super.key});
 
@@ -12,17 +11,41 @@ class _MyEmailsPageState extends State<MyEmailsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       body: SafeArea(
-        child: Column(
-          children: [
-        
-            Row(
-              children: [
-                
-              ],
-            )
-        
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.arrow_back_ios_new,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        "My Email",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
