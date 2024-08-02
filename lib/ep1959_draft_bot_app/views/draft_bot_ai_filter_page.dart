392e6b1bdd9364794da2d3d1515ee66664a30f43
JAICHANGPARK <aristojeff@gmail.com>
2024-08-02 14:18:17 +0900

    Update draft_bot_ai_filter_page.dart

--- lib/ep1959_draft_bot_app/views/draft_bot_ai_filter_page.dart
+++ lib/ep1959_draft_bot_app/views/draft_bot_ai_filter_page.dart
@@ -12,10 +12,14 @@ class _DraftBotAiFilterPageState extends State<DraftBotAiFilterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[100],
       appBar: AppBar(
         centerTitle: true,
         title: Text("AI Filters"),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
+      
     );
   }
 }
