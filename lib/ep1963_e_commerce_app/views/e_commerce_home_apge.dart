ad39bc811ac888f320f9b51111545c8a1108a56a
JAICHANGPARK <aristojeff@gmail.com>
2024-08-04 10:16:32 +0900

    Update e_commerce_home_apge.dart

--- lib/ep1963_e_commerce_app/views/e_commerce_home_apge.dart
+++ lib/ep1963_e_commerce_app/views/e_commerce_home_apge.dart
@@ -12,43 +12,48 @@ class _ECommerceHomeApgeState extends State<ECommerceHomeApge> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Text("drop market"),
-                Spacer(),
-                Icon(
-                  Icons.my_location_outlined,
-                ),
-                Text(
-                  "Seoul, ROK",
-                )
-              ],
-            ),
-            SizedBox(
-              height: 160,
-              child: Placeholder(),
-            ),
-            SizedBox(
-              height: 100,
-              child: Placeholder(),
-            ),
-            Row(
-              children: [
-                Text(
-                  "Popular items",
-                ),
-                Text(
-                  "View all",
-                ),
-                Icon(
-                  Icons.arrow_forward,
-                ),
-              ],
-            )
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text("drop market"),
+                  Spacer(),
+                  Icon(
+                    Icons.my_location_outlined,
+                  ),
+                  Text(
+                    "Seoul, ROK",
+                  )
+                ],
+              ),
+              SizedBox(
+                height: 160,
+                child: Placeholder(),
+              ),
+              SizedBox(
+                height: 100,
+                child: Placeholder(),
+              ),
+              Row(
+                children: [
+                  Text(
+                    "Popular items",
+                  ),
+                  Text(
+                    "View all",
+                  ),
+                  Icon(
+                    Icons.arrow_forward,
+                  ),
+                ],
+              ),
+              SizedBox(height: 280,
+              child: Placeholder(),)
+            ],
+          ),
         ),
       ),
     );
