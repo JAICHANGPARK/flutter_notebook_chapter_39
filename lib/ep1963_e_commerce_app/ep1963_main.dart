f6cda3c303b268b73dee2031492b92db6afe9730
JAICHANGPARK <aristojeff@gmail.com>
2024-08-04 10:11:00 +0900

    Create ep1963_main.dart

---
+++ lib/ep1963_e_commerce_app/ep1963_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class ECommerceApp extends StatelessWidget {
+  const ECommerceApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
