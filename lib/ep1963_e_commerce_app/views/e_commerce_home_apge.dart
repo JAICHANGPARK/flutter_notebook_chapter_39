61d4c0bd19ed621a6f3ff52bef7974fac2b570ab
JAICHANGPARK <aristojeff@gmail.com>
2024-08-04 10:11:47 +0900

    Create e_commerce_home_apge.dart

---
+++ lib/ep1963_e_commerce_app/views/e_commerce_home_apge.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+
+class ECommerceHomeApge extends StatefulWidget {
+  const ECommerceHomeApge({super.key});
+
+  @override
+  State<ECommerceHomeApge> createState() => _ECommerceHomeApgeState();
+}
+
+class _ECommerceHomeApgeState extends State<ECommerceHomeApge> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
