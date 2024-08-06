3b1b15951bb59c1fcfba7992f4833407dae97534
JAICHANGPARK <aristojeff@gmail.com>
2024-08-06 22:48:14 +0900

    Create e_commerce_popular_item_page.dart

---
+++ lib/ep1963_e_commerce_app/views/e_commerce_popular_item_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ECommercePopularItemPage extends StatefulWidget {
+  const ECommercePopularItemPage({super.key});
+
+  @override
+  State<ECommercePopularItemPage> createState() => _ECommercePopularItemPageState();
+}
+
+class _ECommercePopularItemPageState extends State<ECommercePopularItemPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
