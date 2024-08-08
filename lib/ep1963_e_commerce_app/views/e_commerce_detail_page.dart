1873207ae48ec197b05335c2745360cb07f43cf5
JAICHANGPARK <aristojeff@gmail.com>
2024-08-08 19:19:24 +0900

    Create e_commerce_detail_page.dart

---
+++ lib/ep1963_e_commerce_app/views/e_commerce_detail_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ECommerceDetailPage extends StatefulWidget {
+  const ECommerceDetailPage({super.key});
+
+  @override
+  State<ECommerceDetailPage> createState() => _ECommerceDetailPageState();
+}
+
+class _ECommerceDetailPageState extends State<ECommerceDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
