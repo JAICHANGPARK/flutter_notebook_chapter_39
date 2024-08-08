a17e0f28a4b8f68b36168deca7e223fbfdccd8e8
JAICHANGPARK <aristojeff@gmail.com>
2024-08-08 20:37:29 +0900

    Update language_learning_home_page.dart

--- lib/ep1970_language_learning_app/views/language_learning_home_page.dart
+++ lib/ep1970_language_learning_app/views/language_learning_home_page.dart
@@ -43,7 +43,8 @@ class _LanguageLearningHomePageState extends State<LanguageLearningHomePage> {
                   ),
                 ),
               ],
-            )
+            ),
+            Text("English for business")
           ],
         ),
       ),
