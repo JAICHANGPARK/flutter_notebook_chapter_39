6bb1f736f8f8ec16336c7b65b8ab37c68e299482
JAICHANGPARK <aristojeff@gmail.com>
2024-08-08 20:23:57 +0900

    Create language_learning_home_page.dart

---
+++ lib/ep1970_language_learning_app/views/language_learning_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LanguageLearningHomePage extends StatefulWidget {
+  const LanguageLearningHomePage({super.key});
+
+  @override
+  State<LanguageLearningHomePage> createState() => _LanguageLearningHomePageState();
+}
+
+class _LanguageLearningHomePageState extends State<LanguageLearningHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
