180144ac73826ca576eab004f013efc48518033c
JAICHANGPARK <aristojeff@gmail.com>
2024-08-08 20:24:09 +0900

    Update ep1970_main.dart

--- lib/ep1970_language_learning_app/ep1970_main.dart
+++ lib/ep1970_language_learning_app/ep1970_main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'views/language_learning_home_page.dart';
+
 void main() {
   runApp(LanguageLearningApp());
 }
@@ -10,6 +12,8 @@ class LanguageLearningApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: LanguageLearningHomePage(),
+    );
   }
 }
