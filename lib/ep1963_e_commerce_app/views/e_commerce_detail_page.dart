98f8472dbb885a9ea5276459a2a35d1e9428d1f9
JAICHANGPARK <aristojeff@gmail.com>
2024-08-08 19:35:00 +0900

    Update e_commerce_detail_page.dart

--- lib/ep1963_e_commerce_app/views/e_commerce_detail_page.dart
+++ lib/ep1963_e_commerce_app/views/e_commerce_detail_page.dart
@@ -26,129 +26,132 @@ class _ECommerceDetailPageState extends State<ECommerceDetailPage> {
           ),
         ],
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 400,
-            child: Placeholder(),
-          ),
-          Text(
-            "DREAM",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.grey,
+      body: Padding(
+        padding: const EdgeInsets.all(24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 400,
+              child: Placeholder(),
             ),
-          ),
-          Text("Tie dye print satin effect shirt"),
-          Row(
-            children: [
-              Text(
-                "\$ 175.00",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
+            Text(
+              "DREAM",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.grey,
               ),
-              Icon(
-                Icons.star,
-                color: Colors.red,
-              ),
-              Text(
-                "4.9",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text("Select size:"),
-            ],
-          ),
-          Row(
-            children: [
-              ...["XS", "S", "M", "L", "XL"].map((e) {
-                return Container(
-                  height: 36,
-                  width: 36,
-                  margin: EdgeInsets.only(right: 4),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey[300]!,
-                      ),
-                      borderRadius: BorderRadius.circular(6)),
-                  // padding: EdgeInsets.all(8),
-                  child: Center(child: Text("$e")),
-                );
-              }),
-              Gap(12),
-              Text(
-                "Size Guide",
-                style: TextStyle(
-                  color: Colors.red,
-                  decoration: TextDecoration.underline,
-                  decorationColor: Colors.red,
-                ),
-              ),
-              Gap(5),
-              Icon(
-                Icons.zoom_out_map,
-                color: Colors.red,
-              ),
-            ],
-          ),
-          Text(
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
             ),
-          ),
-          SizedBox(
-            height: 64,
-            child: Row(
+            Text("Tie dye print satin effect shirt"),
+            Row(
               children: [
-                Expanded(
-                    child: Container(
-                  decoration: BoxDecoration(
+                Text(
+                  "\$ 175.00",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Icon(
+                  Icons.star,
+                  color: Colors.red,
+                ),
+                Text(
+                  "4.9",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text("Select size:"),
+              ],
+            ),
+            Row(
+              children: [
+                ...["XS", "S", "M", "L", "XL"].map((e) {
+                  return Container(
+                    height: 36,
+                    width: 36,
+                    margin: EdgeInsets.only(right: 4),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey[300]!,
+                        ),
+                        borderRadius: BorderRadius.circular(6)),
+                    // padding: EdgeInsets.all(8),
+                    child: Center(child: Text("$e")),
+                  );
+                }),
+                Gap(12),
+                Text(
+                  "Size Guide",
+                  style: TextStyle(
                     color: Colors.red,
-                    borderRadius: BorderRadius.circular(32),
+                    decoration: TextDecoration.underline,
+                    decorationColor: Colors.red,
                   ),
-                  child: Center(
-                    child: Text(
-                      "Buy now",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                )),
-                Expanded(
-                  child: Container(
-                    decoration: ShapeDecoration(
-                      shape: StadiumBorder(),
-                      color: Colors.black,
-                    ),
-                    child: Center(
-
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Icon(Icons.shopping_cart_outlined,
-                          color: Colors.white,),
-                          Gap(8),
-                          Text(
-                            "Add to cart",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
+                ),
+                Gap(5),
+                Icon(
+                  Icons.zoom_out_map,
+                  color: Colors.red,
                 ),
               ],
             ),
-          )
-        ],
+            Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 64,
+              child: Row(
+                children: [
+                  Expanded(
+                      child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(32),
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Buy now",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  )),
+                  Expanded(
+                    child: Container(
+                      decoration: ShapeDecoration(
+                        shape: StadiumBorder(),
+                        color: Colors.black,
+                      ),
+                      child: Center(
+
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(Icons.shopping_cart_outlined,
+                            color: Colors.white,),
+                            Gap(8),
+                            Text(
+                              "Add to cart",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
