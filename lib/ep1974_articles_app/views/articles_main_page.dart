8e017d272879b278213fdee715619d4694d63209
JAICHANGPARK <aristojeff@gmail.com>
2024-08-15 09:20:25 +0900

    Update articles_main_page.dart

--- lib/ep1974_articles_app/views/articles_main_page.dart
+++ lib/ep1974_articles_app/views/articles_main_page.dart
@@ -14,7 +14,7 @@ class _ArticlesMainPageState extends State<ArticlesMainPage> {
       appBar: AppBar(
 
         centerTitle: true,
-        
+
         title: Text("News & Articles"),
         actions: [
           IconButton(
