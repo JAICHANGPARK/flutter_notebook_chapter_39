7ceeaaccc288c7a28c5f13e9bd6392e5b9daba0c
JAICHANGPARK <aristojeff@gmail.com>
2024-08-15 09:16:03 +0900

    Update articles_main_page.dart

---
+++ lib/ep1974_articles_app/views/articles_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ArticlesMainPage extends StatefulWidget {
+  const ArticlesMainPage({super.key});
+
+  @override
+  State<ArticlesMainPage> createState() => _ArticlesMainPageState();
+}
+
+class _ArticlesMainPageState extends State<ArticlesMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
