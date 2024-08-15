7c19bdc779345885c0a9c8f20c11c84002674e18
JAICHANGPARK <aristojeff@gmail.com>
2024-08-15 09:17:39 +0900

    Update ep1974_main.dart

--- lib/ep1974_articles_app/ep1974_main.dart
+++ lib/ep1974_articles_app/ep1974_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_39/ep1974_articles_app/views/articles_main_page.dart';
 
 void main() {
   runApp(ArticlesApp());
@@ -10,7 +11,7 @@ class ArticlesApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: ArticlesMainPage(),
     );
   }
 }
