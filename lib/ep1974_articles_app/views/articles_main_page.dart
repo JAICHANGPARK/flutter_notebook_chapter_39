2759413bed4a4c63db821d3fd413fbb50a8aefb5
JAICHANGPARK <aristojeff@gmail.com>
2024-08-15 09:19:20 +0900

    Update articles_main_page.dart

--- lib/ep1974_articles_app/views/articles_main_page.dart
+++ lib/ep1974_articles_app/views/articles_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ArticlesMainPage extends StatefulWidget {
   const ArticlesMainPage({super.key});
 
@@ -12,6 +11,18 @@ class _ArticlesMainPageState extends State<ArticlesMainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("News & Articles"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.tune,
+            ),
+          )
+        ],
+      ),
       body: Column(),
     );
   }
