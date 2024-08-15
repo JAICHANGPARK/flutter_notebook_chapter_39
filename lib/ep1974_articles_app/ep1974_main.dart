c3393c093b1d908a826a59fe6f89cbd6c48d7093
JAICHANGPARK <aristojeff@gmail.com>
2024-08-15 09:14:29 +0900

    Create ep1974_main.dart

---
+++ lib/ep1974_articles_app/ep1974_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(ArticlesApp());
+}
+
+class ArticlesApp extends StatelessWidget {
+  const ArticlesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
