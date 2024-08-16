38ec101c20f812e24f13134b6db55bfb3012543c
JAICHANGPARK <aristojeff@gmail.com>
2024-08-16 18:08:57 +0900

    Update article_api.dart

--- lib/ep1974_articles_app/api/article_api.dart
+++ lib/ep1974_articles_app/api/article_api.dart
@@ -1,7 +1,13 @@
-
-
 import 'package:dio/dio.dart';
+import 'package:flutter_notebook_chapter_39/ep1974_articles_app/model/articles.dart';
 
-Future getArticles()async{
+Future<List<Articles>> getArticles() async {
   final dio = Dio();
+  final posts = await dio.get(
+    "https://jsonplaceholder.typicode.com/posts",
+  );
+  if (posts.statusCode == 200) {
+    print(posts.data);
+  }
+  return [];
 }
