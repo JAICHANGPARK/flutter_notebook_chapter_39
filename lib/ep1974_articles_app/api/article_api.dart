8a082596ea3d8e8553eda075c0aa3c7d38fa1ba9
JAICHANGPARK <aristojeff@gmail.com>
2024-08-16 18:37:12 +0900

    Update article_api.dart

--- lib/ep1974_articles_app/api/article_api.dart
+++ lib/ep1974_articles_app/api/article_api.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:flutter_notebook_chapter_39/ep1974_articles_app/model/articles.dart';
+import 'package:flutter_notebook_chapter_39/ep1974_articles_app/model/photos.dart';
 
 Future<List<Articles>> getArticles() async {
   final dio = Dio();
@@ -13,6 +14,23 @@ Future<List<Articles>> getArticles() async {
 
     List<Articles> results = data2.map((e){
       return $ArticlesFromJson(e);
+    }).toList();
+
+    return results;
+  }
+  return [];
+}
+
+Future getPhotos() async{
+  final dio = Dio();
+  final posts = await dio.get(
+    "https://jsonplaceholder.typicode.com/photos",
+  );
+  if (posts.statusCode == 200) {
+    final data2 = posts.data as List<dynamic>;
+
+    List<Photos> results = data2.map((e){
+      return $PhotosFromJson(e);
     }).toList();
 
     return results;
