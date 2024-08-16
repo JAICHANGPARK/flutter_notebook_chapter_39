4f097db343ed9a3d50e69c2c6760b9f764fa9473
JAICHANGPARK <aristojeff@gmail.com>
2024-08-16 18:05:07 +0900

    Update articles.dart

--- lib/ep1974_articles_app/model/articles.dart
+++ lib/ep1974_articles_app/model/articles.dart
@@ -4,6 +4,9 @@
 //     "title": "nesciunt iure omnis dolorem tempora et accusantium",
 //     "body": "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas"
 //   },
-class Articles {
-  String? userId;
+import 'package:genq/genq.dart';
+
+@genq
+class Articles with _$Articles {
+  factory Articles({int? userId, int? id, String? title, String? body}) = _Articles;
 }
