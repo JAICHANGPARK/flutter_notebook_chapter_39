664936f28fe8683748ced42bd7b492e5e55443fe
Dreamwalker <aristojeff@gmail.com>
2024-08-19 08:59:14 +0800

    Update ep1959_main.dart

--- lib/ep1959_draft_bot_app/ep1959_main.dart
+++ lib/ep1959_draft_bot_app/ep1959_main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_39/ep1959_draft_bot_app/views/draft_bot_home_page.dart';
 
 void main() {
-  runApp(DraftBotApp());
+  runApp(const DraftBotApp());
 }
 
 class DraftBotApp extends StatelessWidget {
@@ -10,7 +10,7 @@ class DraftBotApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: DraftBotHomePage(),
     );
   }
