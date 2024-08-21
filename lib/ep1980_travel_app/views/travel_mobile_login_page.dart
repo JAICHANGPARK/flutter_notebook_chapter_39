8228dd6a23481d5742bc92fde5500a8aef84db71
JAICHANGPARK <aristojeff@gmail.com>
2024-08-21 17:49:03 +0900

    Update travel_mobile_login_page.dart

--- lib/ep1980_travel_app/views/travel_mobile_login_page.dart
+++ lib/ep1980_travel_app/views/travel_mobile_login_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TravelMobileLoginPage extends StatefulWidget {
   const TravelMobileLoginPage({super.key});
 
@@ -11,6 +10,16 @@ class TravelMobileLoginPage extends StatefulWidget {
 class _TravelMobileLoginPageState extends State<TravelMobileLoginPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.network(
+              "https://cdn.pixabay.com/photo/2014/07/10/17/17/hotel-389256_1280.jpg",
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
