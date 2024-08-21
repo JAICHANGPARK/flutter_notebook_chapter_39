cde5401122dea1db2d6b0a3f4ffa7fc70bc9e708
JAICHANGPARK <aristojeff@gmail.com>
2024-08-21 17:45:50 +0900

    Update ep1980_travel_app.dart

---
+++ lib/ep1980_travel_app/ep1980_travel_app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  
+}
+
+
+class TravelMobileApp extends StatelessWidget {
+  const TravelMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
