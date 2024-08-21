0868170a4054c779acc501bc9519fba5b2744ed5
JAICHANGPARK <aristojeff@gmail.com>
2024-08-21 17:46:24 +0900

    Create travel_mobile_login_page.dart

---
+++ lib/ep1980_travel_app/views/travel_mobile_login_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class TravelMobileLoginPage extends StatefulWidget {
+  const TravelMobileLoginPage({super.key});
+
+  @override
+  State<TravelMobileLoginPage> createState() => _TravelMobileLoginPageState();
+}
+
+class _TravelMobileLoginPageState extends State<TravelMobileLoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
