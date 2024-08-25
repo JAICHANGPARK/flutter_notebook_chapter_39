e8639aa481291d3c1663ac8200490fae4e1d7526
JAICHANGPARK <aristojeff@gmail.com>
2024-08-25 09:48:15 +0900

    Create travel_mobile_list_page.dart

---
+++ lib/ep1980_travel_app/views/travel_mobile_list_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class TravelMobileListPage extends StatefulWidget {
+  const TravelMobileListPage({super.key});
+
+  @override
+  State<TravelMobileListPage> createState() => _TravelMobileListPageState();
+}
+
+class _TravelMobileListPageState extends State<TravelMobileListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(),
+    );
+  }
+}
