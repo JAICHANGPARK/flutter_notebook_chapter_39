bd2921aa915e6b7c69dfbda1e481e39d7d9a6f14
JAICHANGPARK <aristojeff@gmail.com>
2024-08-27 18:49:04 +0900

    Update freelance_main_page.dart

--- lib/ep1986_freelance_market_app/views/freelance_main_page.dart
+++ lib/ep1986_freelance_market_app/views/freelance_main_page.dart
@@ -17,53 +17,52 @@ class _FreelanceMainPageState extends State<FreelanceMainPage> {
         child: Stack(
           children: [
             Positioned.fill(
+              left: 16,
+              right: 16,
               child: Column(
                 children: [
                   SizedBox(
                     height: 82,
-                    child: Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.grey[200]!,
-                              borderRadius: BorderRadius.circular(8),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200]!,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              HugeIcons.strokeRoundedNotification01,
                             ),
-                            child: IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                HugeIcons.strokeRoundedNotification01,
+                          ),
+                        ),
+                        Expanded(
+                          child: Center(
+                            child: Text(
+                              "People",
+                              style: TextStyle(
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
-                          Expanded(
-                            child: Center(
-                              child: Text(
-                                "People",
-                                style: TextStyle(
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200]!,
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              HugeIcons.strokeRoundedMenu01,
                             ),
                           ),
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.grey[200]!,
-                              borderRadius: BorderRadius.circular(8),
-                            ),
-                            child: IconButton(
-                              onPressed: () {},
-                              icon: Icon(
-                                HugeIcons.strokeRoundedMenu01,
-                              ),
-                            ),
-                          ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                   Row(
