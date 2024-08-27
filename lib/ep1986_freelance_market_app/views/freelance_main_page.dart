877459dacc845c21be4caa17c56942b2037f9dcf
JAICHANGPARK <aristojeff@gmail.com>
2024-08-27 18:40:52 +0900

    Create freelance_main_page.dart

---
+++ lib/ep1986_freelance_market_app/views/freelance_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FreelanceMainPage extends StatefulWidget {
+  const FreelanceMainPage({super.key});
+
+  @override
+  State<FreelanceMainPage> createState() => _FreelanceMainPageState();
+}
+
+class _FreelanceMainPageState extends State<FreelanceMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
