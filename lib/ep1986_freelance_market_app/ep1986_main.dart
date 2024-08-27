1203f1ec2e90ec1af15e4b74e2d66f3cbc4a62db
JAICHANGPARK <aristojeff@gmail.com>
2024-08-27 18:38:36 +0900

    Update ep1986_main.dart

---
+++ lib/ep1986_freelance_market_app/ep1986_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class FreelanceMarketApp extends StatelessWidget {
+  const FreelanceMarketApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
