d709678ded4847b42c02c7d44a2f70e52871a2c9
JAICHANGPARK <aristojeff@gmail.com>
2024-08-28 19:46:29 +0900

    Update freelance_main_page.dart

--- lib/ep1986_freelance_market_app/views/freelance_main_page.dart
+++ lib/ep1986_freelance_market_app/views/freelance_main_page.dart
@@ -171,6 +171,20 @@ class _FreelanceMainPageState extends State<FreelanceMainPage> {
                                       ),
                                     )
                                   ],
+                                ),
+                                Text(
+                                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                                  maxLines: 3,
+                                ),
+                                Row(
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        color: Colors.black,
+                                      ),
+                                      child: Text("\$12"),
+                                    )
+                                  ],
                                 )
                               ],
                             ),
