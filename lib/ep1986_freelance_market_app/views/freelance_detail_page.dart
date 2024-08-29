de488eb7927434184c8b94c4ce660b7153c7b3a6
JAICHANGPARK <aristojeff@gmail.com>
2024-08-29 19:01:27 +0900

    Update freelance_detail_page.dart

--- lib/ep1986_freelance_market_app/views/freelance_detail_page.dart
+++ lib/ep1986_freelance_market_app/views/freelance_detail_page.dart
@@ -39,8 +39,9 @@ class _FreelanceDetailPageState extends State<FreelanceDetailPage> {
                           ),
                         ),
                         Container(
-                          decoration: const BoxDecoration(
-                            color: Colors.lightGreenAccent, borderRadius: BorderRadius.circular(8),
+                          decoration: BoxDecoration(
+                            color: Colors.lightGreenAccent,
+                            borderRadius: BorderRadius.circular(8),
                           ),
                           child: IconButton(
                             icon: const Icon(
@@ -86,7 +87,28 @@ class _FreelanceDetailPageState extends State<FreelanceDetailPage> {
                   )
                 ],
               ),
-            )
+            ),
+            Container(
+              height: 42,
+              child: Placeholder(),
+            ),
+            const Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+              maxLines: 3,
+            ),
+            Row(
+              children: [
+                Text("Read more"),
+                Icon(Icons.arrow_forward),
+              ],
+            ),
+            Container(
+              height: 72,
+              color: Colors.white,
+            ),
+            Expanded(
+              child: Placeholder(),
+            ),
           ],
         ),
       ),
