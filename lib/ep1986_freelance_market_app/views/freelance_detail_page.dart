bd6381aec67ef09f0edd8b88dca17f07389f53b1
JAICHANGPARK <aristojeff@gmail.com>
2024-08-29 18:49:46 +0900

    Update freelance_detail_page.dart

---
+++ lib/ep1986_freelance_market_app/views/freelance_detail_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class FreelanceDetailPage extends StatefulWidget {
+  const FreelanceDetailPage({super.key});
+
+  @override
+  State<FreelanceDetailPage> createState() => _FreelanceDetailPageState();
+}
+
+class _FreelanceDetailPageState extends State<FreelanceDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
