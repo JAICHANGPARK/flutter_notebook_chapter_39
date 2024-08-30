5c549a3c0e5109c7c2b8be142c75ba17accd6542
JAICHANGPARK <aristojeff@gmail.com>
2024-08-30 17:29:14 +0900

    Update freelance_detail_page.dart

--- lib/ep1986_freelance_market_app/views/freelance_detail_page.dart
+++ lib/ep1986_freelance_market_app/views/freelance_detail_page.dart
@@ -95,7 +95,21 @@ class _FreelanceDetailPageState extends State<FreelanceDetailPage> {
                   Container(
                     decoration: ShapeDecoration(
                       shape: StadiumBorder(),
-                      color: Colors.grey[400]!,
+                      color: Colors.grey[200]!,
+                    ),
+                    padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    child: Row(
+                      children: [
+                        Icon(Icons.star_outline),
+                        Gap(2),
+                        Text("4.3"),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    decoration: ShapeDecoration(
+                      shape: StadiumBorder(),
+                      color: Colors.grey[200]!,
                     ),
                     padding: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
                     child: Row(
