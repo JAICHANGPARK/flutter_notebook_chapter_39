b829a8d06299731227786b127a16efaec9c11716
JAICHANGPARK <aristojeff@gmail.com>
2024-08-31 09:42:54 +0900

    Create freelance_chat_page.dart

---
+++ lib/ep1986_freelance_market_app/views/freelance_chat_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class FreelanceChatPage extends StatefulWidget {
+  const FreelanceChatPage({super.key});
+
+  @override
+  State<FreelanceChatPage> createState() => _FreelanceChatPageState();
+}
+
+class _FreelanceChatPageState extends State<FreelanceChatPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+        children: [
+          Row(
+            children: [
+              CircleAvatar(),
+              CircleAvatar(),
+              Column(
+                children: [
+                  Text("Sandra Walker"),
+                  Container(
+                    child: Text(
+                      "Online",
+                    ),
+                  )
+                ],
+              ),
+              Spacer(),
+              Container(
+                decoration: BoxDecoration(),
+                child: IconButton(onPressed: (){}, icon: Icon(Icons.more_horiz),
+              ))
+            ],
+          )
+        ],
+      )),
+    );
+  }
+}
