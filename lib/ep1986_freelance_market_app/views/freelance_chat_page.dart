748e87a33aa352cc7a7a4e6b2b99c1c11eae59ac
JAICHANGPARK <aristojeff@gmail.com>
2024-08-31 09:48:45 +0900

    Update freelance_chat_page.dart

--- lib/ep1986_freelance_market_app/views/freelance_chat_page.dart
+++ lib/ep1986_freelance_market_app/views/freelance_chat_page.dart
@@ -60,31 +60,67 @@ class _FreelanceChatPageState extends State<FreelanceChatPage> {
               ),
               Gap(16),
               Expanded(
-                  child: Column(
-                children: [
-                  Container(
-                    child: Text("Thu, 27 Jun"),
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(width: 64,),
-                      Expanded(
-                        child: Column(
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                color: Colors.lightGreenAccent,
-                              ),
-                            ),
-                            Text("11:35 am")
-                          ],
+                child: Column(
+                  children: [
+                    Container(
+                      child: Text("Thu, 27 Jun"),
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 64,
                         ),
-                      ),
-                      
-                    ],
-                  )
-                ],
-              )),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.lightGreenAccent,
+                                ),
+                                child: Text(
+                                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                                ),
+                              ),
+                              Text("11:35 am")
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        CircleAvatar(),
+                        Expanded(
+                          child: Column(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Text(
+                                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                                  maxLines: 3,
+                                ),
+                              ),
+                              Gap(12),
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.lightGreenAccent,
+                                ),
+                                child: Text(
+                                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                                ),
+                              ),
+                              Text("11:35 am")
+                            ],
+                          ),
+                        ),
+                        SizedBox(width: 64),
+                      ],
+                    )
+                  ],
+                ),
+              ),
               Gap(16),
               Row(
                 children: [
