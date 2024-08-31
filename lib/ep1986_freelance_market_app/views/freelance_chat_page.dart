e590e0fd82c2ef051b86c32678d9b54667d363ba
JAICHANGPARK <aristojeff@gmail.com>
2024-08-31 09:49:05 +0900

    Update freelance_chat_page.dart

--- lib/ep1986_freelance_market_app/views/freelance_chat_page.dart
+++ lib/ep1986_freelance_market_app/views/freelance_chat_page.dart
@@ -87,7 +87,7 @@ class _FreelanceChatPageState extends State<FreelanceChatPage> {
                         ),
                       ],
                     ),
-                    Row(
+                    Row(crossAxisAlignment: CrossAxisAlignment.end,
                       children: [
                         CircleAvatar(),
                         Expanded(
@@ -105,7 +105,7 @@ class _FreelanceChatPageState extends State<FreelanceChatPage> {
                               Gap(12),
                               Container(
                                 decoration: BoxDecoration(
-                                  color: Colors.lightGreenAccent,
+                                  color: Colors.white,
                                 ),
                                 child: Text(
                                   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
