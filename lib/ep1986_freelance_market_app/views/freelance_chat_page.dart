cc4a46017215e35f898806d7d79244326c572c30
JAICHANGPARK <aristojeff@gmail.com>
2024-08-31 09:44:47 +0900

    Update freelance_chat_page.dart

--- lib/ep1986_freelance_market_app/views/freelance_chat_page.dart
+++ lib/ep1986_freelance_market_app/views/freelance_chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class FreelanceChatPage extends StatefulWidget {
   const FreelanceChatPage({super.key});
@@ -12,37 +13,51 @@ class _FreelanceChatPageState extends State<FreelanceChatPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                CircleAvatar(),
-                CircleAvatar(),
-                Column(
-                  children: [
-                    Text("Sandra Walker"),
-                    Container(
-                      child: Text(
-                        "Online",
-                      ),
-                    )
-                  ],
-                ),
-                Spacer(),
-                Container(
-                  decoration: BoxDecoration(),
-                  child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(Icons.more_horiz),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(),
+                  CircleAvatar(),
+                  Column(
+                    children: [
+                      Text("Sandra Walker"),
+                      Container(
+                        child: Text(
+                          "Online",
+                        ),
+                      )
+                    ],
                   ),
-                )
-              ],
-            )
-          ],
+                  Spacer(),
+                  Container(
+                    decoration: BoxDecoration(),
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.more_horiz),
+                    ),
+                  )
+                ],
+              ),
+              Expanded(child: Placeholder()),
+              Row(
+                children: [
+                  Expanded(child: Container(
+                      decoration: BoxDecoration(),
+                      child: TextField())),
+                  Gap(8)
+                  CircleAvatar(
+                    backgroundColor: Colors.lightGreenAccent,
+                    child: Icon(Icons.send_outlined),
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
-      )),
+      ),
     );
   }
 }
