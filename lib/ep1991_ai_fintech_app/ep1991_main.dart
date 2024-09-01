5f72f9ee5875a2d01f07aa3f427b9f3bda79775f
JAICHANGPARK <aristojeff@gmail.com>
2024-09-01 11:29:18 +0900

    Create ep1991_main.dart

---
+++ lib/ep1991_ai_fintech_app/ep1991_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class AiFintechApp extends StatelessWidget {
+  const AiFintechApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
