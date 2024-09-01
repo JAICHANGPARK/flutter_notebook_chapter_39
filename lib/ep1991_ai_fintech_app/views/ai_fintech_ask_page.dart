9a5218cc6278b649a3085182eeb2f34734c71d05
JAICHANGPARK <aristojeff@gmail.com>
2024-09-01 11:30:40 +0900

    Update ai_fintech_ask_page.dart

---
+++ lib/ep1991_ai_fintech_app/views/ai_fintech_ask_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class AiFintechAskPage extends StatefulWidget {
+  const AiFintechAskPage({super.key});
+
+  @override
+  State<AiFintechAskPage> createState() => _AiFintechAskPageState();
+}
+
+class _AiFintechAskPageState extends State<AiFintechAskPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
