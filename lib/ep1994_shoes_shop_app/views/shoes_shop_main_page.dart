912c3a42845b549213f061b0b574fe554b274f04
JAICHANGPARK <aristojeff@gmail.com>
2024-09-04 20:02:07 +0900

    Create shoes_shop_main_page.dart

---
+++ lib/ep1994_shoes_shop_app/views/shoes_shop_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ShoesShopMainPage extends StatefulWidget {
+  const ShoesShopMainPage({super.key});
+
+  @override
+  State<ShoesShopMainPage> createState() => _ShoesShopMainPageState();
+}
+
+class _ShoesShopMainPageState extends State<ShoesShopMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
