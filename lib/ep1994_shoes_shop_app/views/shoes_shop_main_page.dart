5e070f818433f3e7be964d03a5daea352867b752
JAICHANGPARK <aristojeff@gmail.com>
2024-09-04 20:03:56 +0900

    Update shoes_shop_main_page.dart

--- lib/ep1994_shoes_shop_app/views/shoes_shop_main_page.dart
+++ lib/ep1994_shoes_shop_app/views/shoes_shop_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class ShoesShopMainPage extends StatefulWidget {
   const ShoesShopMainPage({super.key});
 
@@ -11,6 +10,20 @@ class ShoesShopMainPage extends StatefulWidget {
 class _ShoesShopMainPageState extends State<ShoesShopMainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: CircleAvatar(
+          backgroundColor: Colors.grey[100],
+          child: Icon(Icons.menu),
+        ),
+        title: Text("Logo"),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
