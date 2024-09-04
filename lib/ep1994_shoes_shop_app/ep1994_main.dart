c5499f54c18a5dbfbacf4008d7c5a1ab35a4103e
JAICHANGPARK <aristojeff@gmail.com>
2024-09-04 19:59:05 +0900

    Update ep1994_main.dart

--- lib/ep1994_shoes_shop_app/ep1994_main.dart
+++ lib/ep1994_shoes_shop_app/ep1994_main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
-class Ep1994Main extends StatelessWidget {
-  const Ep1994Main({Key? key}) : super(key: key);
+void main() {
+  runApp(ShoesShopApp());
+}
+
+class ShoesShopApp extends StatelessWidget {
+  const ShoesShopApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
