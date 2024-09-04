3e52dd4e8768b0d3d9e3247052d3ca98f48c8a75
JAICHANGPARK <aristojeff@gmail.com>
2024-09-04 19:58:38 +0900

    Create ep1994_main.dart

---
+++ lib/ep1994_shoes_shop_app/ep1994_main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Ep1994Main extends StatelessWidget {
+  const Ep1994Main({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
