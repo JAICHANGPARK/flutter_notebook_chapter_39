6919217961a44e61a57401a378f9ba4198e215f5
JAICHANGPARK <aristojeff@gmail.com>
2024-09-05 19:59:46 +0900

    Update shoes_shop_main_page.dart

--- lib/ep1994_shoes_shop_app/views/shoes_shop_main_page.dart
+++ lib/ep1994_shoes_shop_app/views/shoes_shop_main_page.dart
@@ -94,95 +94,95 @@ class _ShoesShopMainPageState extends State<ShoesShopMainPage> {
                           children: [
                             Expanded(
                                 child: Container(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Expanded(
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        border: Border.all(
-                                          color: Colors.grey[200]!,
-                                        ),
-                                        borderRadius: BorderRadius.circular(8),
-                                      ),
-                                      child: Stack(
-                                        children: [
-                                          Positioned(
-                                            right: 4,
-                                            top: 4,
-                                            child: CircleAvatar(
-                                              child: Icon(
-                                                Icons.favorite_outline,
-                                              ),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Expanded(
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            border: Border.all(
+                                              color: Colors.grey[200]!,
                                             ),
-                                          )
-                                        ],
+                                            borderRadius: BorderRadius.circular(8),
+                                          ),
+                                          child: Stack(
+                                            children: [
+                                              Positioned(
+                                                right: 4,
+                                                top: 4,
+                                                child: CircleAvatar(
+                                                  child: Icon(
+                                                    Icons.favorite_outline,
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
                                       ),
-                                    ),
+                                      Gap(12),
+                                      Text(
+                                        "PREMIUM SHOES",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        "\$174.00",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                        ),
+                                      )
+                                    ],
                                   ),
-                                  Gap(12),
-                                  Text(
-                                    "PREMIUM SHOES",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    "\$174.00",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            )),
+                                )),
                             Gap(12),
                             Expanded(
                                 child: Container(
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Expanded(
-                                    child: Container(
-                                      decoration: BoxDecoration(
-                                        border: Border.all(
-                                          color: Colors.grey[200]!,
-                                        ),
-                                        borderRadius: BorderRadius.circular(8),
-                                      ),
-                                      child: Stack(
-                                        children: [
-                                          Positioned(
-                                            right: 4,
-                                            top: 4,
-                                            child: CircleAvatar(
-                                              child: Icon(
-                                                Icons.favorite_outline,
-                                              ),
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Expanded(
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            border: Border.all(
+                                              color: Colors.grey[200]!,
                                             ),
-                                          )
-                                        ],
+                                            borderRadius: BorderRadius.circular(8),
+                                          ),
+                                          child: Stack(
+                                            children: [
+                                              Positioned(
+                                                right: 4,
+                                                top: 4,
+                                                child: CircleAvatar(
+                                                  child: Icon(
+                                                    Icons.favorite_outline,
+                                                  ),
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                        ),
                                       ),
-                                    ),
+                                      Gap(12),
+                                      Text(
+                                        "PREMIUM SHOES",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Text(
+                                        "\$174.00",
+                                        style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18,
+                                        ),
+                                      )
+                                    ],
                                   ),
-                                  Gap(12),
-                                  Text(
-                                    "PREMIUM SHOES",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    "\$174.00",
-                                    style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18,
-                                    ),
-                                  )
-                                ],
-                              ),
-                            )),
+                                )),
                           ],
                         ),
                       ),
@@ -208,9 +208,20 @@ class _ShoesShopMainPageState extends State<ShoesShopMainPage> {
                           ),
                         ],
                       ),
+                      Gap(24),
                       SizedBox(
                         height: 42,
-                        child: Placeholder(),
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              decoration: BoxDecoration(
+                                color: Colors.deepOrangeAccent,
+                              ),
+                              child: Text("All"),
+                            );
+                          },
+                        ),
                       ),
                       GridView.count(
                         shrinkWrap: true,
