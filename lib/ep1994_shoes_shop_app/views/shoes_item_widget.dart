338233dbf0347ab90d99aa576e1757817520c6bc
JAICHANGPARK <aristojeff@gmail.com>
2024-09-05 20:01:20 +0900

    Update shoes_item_widget.dart

---
+++ lib/ep1994_shoes_shop_app/views/shoes_item_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ShoesItemWidget extends StatelessWidget {
+  const ShoesItemWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
