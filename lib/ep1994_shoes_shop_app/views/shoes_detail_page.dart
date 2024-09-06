f4e989cd5464927ed918ccb2d00e9b48f20b0cc1
JAICHANGPARK <aristojeff@gmail.com>
2024-09-06 18:01:50 +0900

    Update shoes_detail_page.dart

--- lib/ep1994_shoes_shop_app/views/shoes_detail_page.dart
+++ lib/ep1994_shoes_shop_app/views/shoes_detail_page.dart
@@ -10,6 +10,14 @@ class ShoesDetailPage extends StatefulWidget {
 class _ShoesDetailPageState extends State<ShoesDetailPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+
+          ],
+        ),
+      ),
+    );
   }
 }
