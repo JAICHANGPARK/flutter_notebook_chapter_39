f385cdbabffba9d0187e62b83989729f694435ae
JAICHANGPARK <aristojeff@gmail.com>
2024-09-06 18:02:26 +0900

    Update shoes_item_widget.dart

--- lib/ep1994_shoes_shop_app/views/shoes_item_widget.dart
+++ lib/ep1994_shoes_shop_app/views/shoes_item_widget.dart
@@ -1,53 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
+import 'shoes_detail_page.dart';
+
 class ShoesItemWidget extends StatelessWidget {
   const ShoesItemWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.grey[200]!,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => ShoesDetailPage(),
+          ),
+        );
+      },
+      child: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.grey[200]!,
+                  ),
+                  borderRadius: BorderRadius.circular(8),
                 ),
-                borderRadius: BorderRadius.circular(8),
-              ),
-              child: Stack(
-                children: [
-                  Positioned(
-                    right: 4,
-                    top: 4,
-                    child: CircleAvatar(
-                      child: Icon(
-                        Icons.favorite_outline,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      right: 4,
+                      top: 4,
+                      child: CircleAvatar(
+                        child: Icon(
+                          Icons.favorite_outline,
+                        ),
                       ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          Gap(12),
-          Text(
-            "PREMIUM SHOES",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
+            Gap(12),
+            Text(
+              "PREMIUM SHOES",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
             ),
-          ),
-          Text(
-            "\$174.00",
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 18,
-            ),
-          )
-        ],
+            Text(
+              "\$174.00",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
