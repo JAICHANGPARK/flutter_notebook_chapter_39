45d18d9c7350de64d694766185a937d88736268e
JAICHANGPARK <aristojeff@gmail.com>
2024-09-06 18:06:20 +0900

    Update shoes_detail_page.dart

--- lib/ep1994_shoes_shop_app/views/shoes_detail_page.dart
+++ lib/ep1994_shoes_shop_app/views/shoes_detail_page.dart
@@ -15,7 +15,7 @@ class _ShoesDetailPageState extends State<ShoesDetailPage> {
         child: Column(
           children: [
             Container(
-              height: 400,
+              height: 360,
               color: Colors.white,
               child: Column(
                 children: [
@@ -49,10 +49,46 @@ class _ShoesDetailPageState extends State<ShoesDetailPage> {
               ),
             ),
             Expanded(
-              child: Placeholder(),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.deepOrange[50],
+                ),
+              ),
             )
           ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_filled,
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: "Cart",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite_outline,
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_3_outlined,
+            ),
+            label: "Home",
+          )
+        ],
       ),
     );
   }
