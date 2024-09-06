1d6b5114315760f5ca9f3ecca5e04b4e3c2b95f8
JAICHANGPARK <aristojeff@gmail.com>
2024-09-06 18:05:04 +0900

    Update shoes_detail_page.dart

--- lib/ep1994_shoes_shop_app/views/shoes_detail_page.dart
+++ lib/ep1994_shoes_shop_app/views/shoes_detail_page.dart
@@ -17,7 +17,37 @@ class _ShoesDetailPageState extends State<ShoesDetailPage> {
             Container(
               height: 400,
               color: Colors.white,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 20,
+                        backgroundColor: Colors.grey[200],
+                        child: Icon(
+                          Icons.arrow_back_ios_new_outlined,
+                        ),
+                      ),
+                      Expanded(
+                        child: Center(
+                          child: Text("Products Details"),
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: 20,
+                        backgroundColor: Colors.grey[200],
+                        child: Icon(
+                          Icons.arrow_back_ios_new_outlined,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
+            Expanded(
+              child: Placeholder(),
+            )
           ],
         ),
       ),
