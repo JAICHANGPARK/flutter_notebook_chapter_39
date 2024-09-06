1f09f397e0370e18cc0ad819a82b8c3759ead4c5
JAICHANGPARK <aristojeff@gmail.com>
2024-09-06 18:01:30 +0900

    Update shoes_detail_page.dart

---
+++ lib/ep1994_shoes_shop_app/views/shoes_detail_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoesDetailPage extends StatefulWidget {
+  const ShoesDetailPage({super.key});
+
+  @override
+  State<ShoesDetailPage> createState() => _ShoesDetailPageState();
+}
+
+class _ShoesDetailPageState extends State<ShoesDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
