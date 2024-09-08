3a37833a0528595a779a1cd310092ba641727235
JAICHANGPARK <aristojeff@gmail.com>
2024-09-08 10:48:03 +0900

    Update shoes_cart_page.dart

---
+++ lib/ep1994_shoes_shop_app/views/shoes_cart_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShoesCartPage extends StatefulWidget {
+  const ShoesCartPage({super.key});
+
+  @override
+  State<ShoesCartPage> createState() => _ShoesCartPageState();
+}
+
+class _ShoesCartPageState extends State<ShoesCartPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
