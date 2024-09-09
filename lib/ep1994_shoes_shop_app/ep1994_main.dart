c188cf9fc5cddd3256555e0b1461bb40c00d69a3
JAICHANGPARK <aristojeff@gmail.com>
2024-09-09 19:52:13 +0900

    Update ep1994_main.dart

--- lib/ep1994_shoes_shop_app/ep1994_main.dart
+++ lib/ep1994_shoes_shop_app/ep1994_main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_39/ep1994_shoes_shop_app/views/shoes_shop_main_page.dart';
-
+import 'package:signals/signals.dart';
 void main() {
+  SignalsObserver.instance = LoggingSignalsObserver();
   runApp(ShoesShopApp());
 }
 
